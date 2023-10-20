f6aaf8a116a7ce3506b4e62648280d609d3af363
Kirill Mashina <you@example.com>
2023-10-20 17:50:24 +0300

    Version 1.5

--- utils/files.py
+++ utils/files.py
@@ -10,21 +10,17 @@ from sqlalchemy.orm import Session
 async def save_document(bot: Bot, user_id: int, document: PhotoSize|Document) -> str:
     file: File = await bot.get_file(document.file_id)
     file_name = file.file_id
-    date_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    directory = f"files/{user_id}/{date_time}"
-    path = directory + f"/{file_name}"
-    os.makedirs(directory)
-
+    path = f"files/{file_name}"
     await bot.download_file(file.file_path, path)
 
-    return f"{user_id}/{date_time}/{file_name}"
+    return file_name
 
 
 async def get_document(user_id: int, file: str, user_files_path: str) -> FSInputFile:
     """
     :return: пользовательский файл с резюме в виде FSInputFile
     """
-    path = f"{user_files_path}/{user_id}/{file}"
+    path = f"files/{file}"
     return FSInputFile(path)
 
